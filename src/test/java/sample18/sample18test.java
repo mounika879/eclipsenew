6e7bea52a7fbf78791c57d1c038c8644b5c0ba0c
mounika <kodatimounika@gmail.com>
2019-05-02 20:09:55 +0530

    this is sample18

---
+++ src/test/java/sample18/sample18test.java
@@ -0,0 +1,14 @@
+package sample18;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample18test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
